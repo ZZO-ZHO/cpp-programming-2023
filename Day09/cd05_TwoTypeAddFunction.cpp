3bbc8a645f720629275c33dd5b14c6a5a079295b
jeong jaeyeong <87115859+ZZO-ZHO@users.noreply.github.com>
2023-03-31 17:23:01 +0900

    Add files via upload

---
+++ Day09/cd05_TwoTypeAddFunction.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+template <typename T>
+T Add(T num1, T num2)
+{
+	cout << "TAdd(T num1, T num2)" << endl;
+	return num1 + num2;
+}
+
+int Add(int num1, int num2)
+{
+	cout << "Add (int num1, int num2)" << endl;
+	return num1 + num2;
+}
+
+int main(void)
+{
+	cout << Add(5, 7) << endl;
+	cout << Add(3.7, 7.5) << endl;
+	cout << Add<int>(5, 7) << endl;
+	cout << Add<double>(3.7, 7.5) << endl;
+	return 0;
+}
